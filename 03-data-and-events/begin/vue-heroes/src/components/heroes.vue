49aa6334dcc1673fbf7e6be4f75b78d70579b4d2
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-26 20:55:09 -0300

    add events onclick

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -66,17 +66,25 @@
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button 
+              class="link card-footer-item cancel-button"
+              @click="cancelHero"
+            >
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button 
+              class="link card-footer-item save-button" 
+              @click="saveHero"
+            >
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info">{{ message }}</div>
+        <div class="notification is-info">
+          <pre>{{ message }}</pre> 
+        </div>
       </div>
     </div>
   </div>
@@ -99,5 +107,15 @@ export default {
       message: 'Im a hero',
     };
   },
+  methods: {
+    
+    cancelHero() {
+      this.message = '';
+    },
+
+    saveHero() {
+      this.message = JSON.stringify(this.hero, null, '\n');
+    },
+  }
 };
 </script>
