b88009352be6c0e5228bf5b8bba889cd343d9589
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-12-13 16:01:21 -0300

    add show and hide with v-show

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -46,6 +46,16 @@
               />
             </div>
             <div class="field">
+              <label for="show" class="checkbox">
+                Show more
+                <input 
+                  type="checkbox" 
+                  class="is-primary" 
+                  id="show" 
+                  v-model="showMore">
+              </label>
+            </div>
+            <div class="field" v-show="showMore">
               <label class="label" for="lastName">last name</label>
               <input
                 class="input"
@@ -53,7 +63,7 @@
                 v-model="selectedHero.lastName"
               />
             </div>
-            <div class="field">
+            <div class="field" v-show="showMore">
               <label class="label" for="description">description</label>
               <input
                 class="input"
@@ -74,6 +84,7 @@ export default {
   data() {
     return {
       selectedHero: undefined,
+      showMore: false,
       heroes: [
         {
           id: 10,
