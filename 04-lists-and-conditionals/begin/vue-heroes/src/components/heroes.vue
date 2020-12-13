4eebcf0ea935013224a48a2e30010dab8be61f3e
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-12-13 15:53:30 -0300

    add content to dom with v-if

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -24,7 +24,7 @@
         </ul>
       </div>
     </div>
-    <div class="columns">
+    <div class="columns" v-if="selectedHero">
       <div class="column is-3">
         <header class="card-header">
           <p class="card-header-title">{{ selectedHero.firstName }}</p>
@@ -73,7 +73,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {},
+      selectedHero: undefined,
       heroes: [
         {
           id: 10,
